e12ff6d9b4bb64c433ec59ac64f021f893c7f11f
“jeonga-Son” <jjeong0106@gmail.com>
2023-04-03 15:35:14 +0900

    apply bootstrap design

--- vue-spring/src/main.js
+++ vue-spring/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.min.css" // [bootstrap]
+import "bootstrap" // [bootstrap]
+
 createApp(App).mount('#app')
